4f35c695d06375ce04013949e19d885ba9e8d5c9
Paul Rodriguez <paul.d.rodriguez@outlook.com>
2021-01-17 22:18:06 -0800

    fix: more fixes to prevent script from installing

--- husky_install.sh
+++ husky_install.sh
@@ -2,11 +2,7 @@
 
 set -e;
 
-result=${PWD##*/}
 
-# prevent from running if script is installed in node modules
-if [ $result != "node_modules" ]; then
-  echo "installing husky"
-  husky install
-  exit 0;
-fi
+echo "installing husky"
+husky install
+exit 0;
