80f054ba4696d9982201f2b1dcd9289d7506cb4d
Paul Rodriguez <paul.d.rodriguez@outlook.com>
2021-01-22 23:00:10 -0800

    fix: removed babel since we are using ts-jest. add more tests

--- src/CountdownTimer/__tests__/CountdownTimer.test.tsx
+++ src/CountdownTimer/__tests__/CountdownTimer.test.tsx
@@ -6,7 +6,15 @@ import CountdownTimer from '../CountdownTimer';
 test('CountdownTimer is initialized correctly', ()=> {
   const timer = shallow(<CountdownTimer minutes={0} seconds={10} />);
 
+  expect(timer).toMatchSnapshot();
+
+})
+
+test('Countdown timer toggles to pause icon when play button is clicked', ()=>{
+  const timer = shallow(<CountdownTimer minutes={0} seconds={10} />);
   expect(timer.find('#play-icon')).toHaveLength(1);
   timer.find("#toggle-play").simulate('click');
   expect(timer.state('seconds')).toBe(10);
-})
+  expect(timer.find('#pause-icon')).toHaveLength(1);
+  expect(timer.find("#play-icon")).not.toBeTruthy();
+});
